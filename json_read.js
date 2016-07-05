44f943740dc0354a08af2fde7fa5dda9c8564306
masterSergey <cyberaktiv@yandex.ru>
2016-07-05 17:27:34 +0300

    Revert "v 1.0"
    This reverts commit 3bbba23263e06282d2b4dbdd5dade539dbe31b88.

--- json_read.js
+++ json_read.js
@@ -1,29 +1,25 @@
 ;(function(win){
 	
-	win.JSON.read = function(json){
-		if(typeof json === "object" && !Array.isArray(json)){
-			return read(json);
-		} else if(typeof json === "string"){
-			return read(JSON.parse(json));
-		}
-		return false;
+	win.JSON.read = function(json_string){
+		return read(JSON.parse(json_string));
 	}
 
 	var read = function(json){
 		for(var k in json){
 			if(typeof json[k] == "string"){
 				switch(json[k]){
-					case "NaN":       json[k] = NaN;       break;
-					case "null":      json[k] = null;      break;
-					case "true":      json[k] = true;      break;
-					case "false":     json[k] = false;     break;
+					case "NaN": 	  json[k] = NaN; 	   break;
+					case "null": 	  json[k] = null;      break;
+					case "true": 	  json[k] = true; 	   break;
+					case "false": 	  json[k] = false;     break;
 					case "Infinity":  json[k] = Infinity;  break;
 					case "-Infinity": json[k] = -Infinity; break;
 					default:
 						var n = parseInt(json[k]);
 						(!isNaN(n))? json[k] = n: json[k];
 						break;
-				}				
+				}
+				console.log(json[k]);
 			} else {
 				read(json[k]);
 			}
