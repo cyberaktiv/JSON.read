176f9c2e874cadaf88337555f922017d13a5dfc8
masterSergey <cyberaktiv@yandex.ru>
2016-07-05 18:09:11 +0300

    v 1.0

--- json_read.js
+++ json_read.js
@@ -1,25 +1,29 @@
 ;(function(win){
 	
-	win.JSON.read = function(json_string){
-		return read(JSON.parse(json_string));
+	win.JSON.read = function(json){
+		if(typeof json === "object" && !Array.isArray(json)){
+			return read(json);
+		} else if(typeof json === "string"){
+			return read(JSON.parse(json));
+		}
+		return false;
 	}
 
 	var read = function(json){
 		for(var k in json){
 			if(typeof json[k] == "string"){
 				switch(json[k]){
-					case "NaN": 	  json[k] = NaN; 	   break;
-					case "null": 	  json[k] = null;      break;
-					case "true": 	  json[k] = true; 	   break;
-					case "false": 	  json[k] = false;     break;
+					case "NaN":       json[k] = NaN;       break;
+					case "null":      json[k] = null;      break;
+					case "true":      json[k] = true;      break;
+					case "false":     json[k] = false;     break;
 					case "Infinity":  json[k] = Infinity;  break;
 					case "-Infinity": json[k] = -Infinity; break;
 					default:
 						var n = parseInt(json[k]);
-						(!isNaN(n))? json[k] = n: json[k];
+						(n.toString().length == json[k].length && !isNaN(n))? json[k] = n : json[k];							
 						break;
-				}
-				console.log(json[k]);
+				}				
 			} else {
 				read(json[k]);
 			}
